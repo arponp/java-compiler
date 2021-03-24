a5c6a774834e15262f125d9e2c4dcead4d5fd764
Arpon Purkayastha <arponp30@gmail.com>
2021-03-23 20:28:44 -0700

    add printing for strings

--- Compiler.java
+++ Compiler.java
@@ -9,27 +9,30 @@ public class Compiler {
             File file = new File("/Users/arponp/Desktop/Mission 20-21/Java mission/java-compiler/sample.txt");
             Scanner input = new Scanner(file);
             while (input.hasNextLine()) {
-                String code = input.nextLine();
-                if (code.startsWith("#")) {
+                String line = input.nextLine();
+                if (line.startsWith("#")) {
                     // commenting
                     continue;
-                } else if (code.startsWith("var")) { // creating variable
-                    if (code.split(" ")[3].startsWith("\"")) { // string
+                } else if (line.startsWith("var")) { // creating variable
+                    if (line.split(" ")[3].startsWith("\"")) { // string
                         String value = "";
-                        for (int i = 3; i < code.split(" ").length; i++) {
-                            if (i == code.split(" ").length - 1) {
-                                value += code.split(" ")[i];
+                        for (int i = 3; i < line.split(" ").length; i++) {
+                            if (i == line.split(" ").length - 1) {
+                                value += line.split(" ")[i];
                             } else {
-                                value += code.split(" ")[i] + " ";
+                                value += line.split(" ")[i] + " ";
                             }
                         }
                         value = value.substring(1, value.length() - 1);
-                        strVars.addVariable(code.split(" ")[1], value);
-                        System.out.println(strVars.getValue(code.split(" ")[1]));
+                        strVars.addVariable(line.split(" ")[1], value);
                     } else { // integer
-                        int value = Integer.parseInt(code.split(" ")[3]);
-                        intVars.addVariable(code.split(" ")[1], value);
-                        System.out.println(intVars.getValue(code.split(" ")[1]));
+                        int value = Integer.parseInt(line.split(" ")[3]);
+                        intVars.addVariable(line.split(" ")[1], value);
+                    }
+                } else if (line.startsWith("print(")) {
+                    String query = line.substring(line.indexOf("(") + 1, line.indexOf(")"));
+                    if (query.startsWith("\"")) {
+                        System.out.println(query.substring(query.indexOf("\"") + 1, query.length() - 1));
                     }
                 }
             }
